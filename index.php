3aadb40c81772f0bdac715269e93a4aa80ef1958
matto <matto@damniam.de>
2017-01-23 11:15:36 +0100

    bludit version of https://github.com/muellmatto/seht-html-mockup

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>
+        <?php echo $Site->title() ?>
+    </title>
+
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo HTML_PATH_THEME_CSS.'normalize.css' ?>" \>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo HTML_PATH_THEME_CSS.'seht.css' ?>" \>
+
+    <script src="http://code.responsivevoice.org/responsivevoice.js">
+    </script>
+
+    <script type="text/javascript">
+
+            function startSpeech() {
+                var text = [];
+                text = document.getElementsByTagName("inhalt");
+                lies = text[0].textContent;
+                responsiveVoice.speak(lies, "Deutsch Female",{
+                    onstart: function(){
+                        document.getElementById('speechIcon').src = "img/schweig.svg";
+                    },
+                    onend: function(){
+                        document.getElementById('speechIcon').src = "img/sprich.svg";
+                    }
+                });
+                
+            }
+        
+            function stopSpeech() {
+                responsiveVoice.cancel();
+            }
+            
+            function toggleSpeech() {
+                if ( !(responsiveVoice.isPlaying()) ) {
+                    startSpeech();
+                } else {
+                    document.getElementById('speechIcon').src = "img/sprich.svg";
+                    stopSpeech();
+                }
+            }
+
+
+            function toggleFont() {
+                var LISTE = document.getElementsByTagName("inhalt")[0].getElementsByTagName("*");
+                // text = document.getElementsByTagName("inhalt")[0];
+                if ( document.getElementById('fontIcon').src.endsWith("img/tT.svg") ) {
+                    document.getElementById('fontIcon').src = "img/Tt.svg";
+                    originalSize = document.getElementsByTagName("html")[0].style.fontSize;
+                    document.getElementsByTagName("html")[0].style.fontSize = "1.76rem";
+                } else {
+                    document.getElementById('fontIcon').src = "img/tT.svg";
+                    document.getElementsByTagName("html")[0].style.fontSize = originalSize;
+                }
+            }
+
+    </script>
+
+</head>
+
+<body>
+
+
+<!-- top bar -->
+    <div class="maxWidth">
+        <div class="sehtrand" style="margin-top: 1rem;">
+            <div style="float: right; padding: 0 0.6rem 0 0; " onclick="toggleSpeech()">
+                <img id="speechIcon" src="<?php echo HTML_PATH_THEME.'img/sprich.svg' ?>" style="width: auto; height: 3rem;" alt="speech icon">
+            </div>
+            <div style="float: right; padding: 0 0.6rem 0 0;" onclick="toggleFont()">
+                <img id="fontIcon" src="<?php echo HTML_PATH_THEME.'img/tT.svg' ?>" style="width: auto; height: 3rem;" alt="font icon">
+            </div>
+            <div style="float: left; padding: 0 0.6rem 1.5rem 0.6rem;">
+                    <a href="/" style="text-decoration: none;">
+                        <img src="<?php echo HTML_PATH_THEME.'img/LOGO.svg' ?>" style="width: auto; height: 3rem;" alt="Logo SeHT Münster e.V.">
+                    </a>
+            </div>
+        </div> 
+    </div>
+<!-- top bar -->
+
+
+
+<!-- navigation -->
+    <div class="maxWidth">
+        <div class="sehtrand"> 
+            <?php
+                $parents = $pagesParents[NO_PARENT_CHAR];
+                foreach($parents as $Parent) {
+                    if ( substr( $Parent->description(), 0, 3 ) == 'fa-' ) {
+                        $icon = 'fa '.$Parent->description();
+                    } else {
+                        $icon = '';
+                    }
+                    if ( $Parent->title() == $Page->title() ) {
+                        $button='sehtButton blau';
+                    } else {
+                        $button='sehtButton';
+                    }
+                    if ( $Parent->title() !='Start' ) {
+                        echo '<div style="float: left; margin: 0 0rem 0 0;">
+                            <a class="'.$button.'" href="'.$Parent->permalink().'">
+                                <i class="'.$icon.'">
+                                </i>
+                                '.$Parent->title().'
+                            </a>
+                        </div>';
+                    }
+                }
+            ?>
+        </div> 
+    </div>
+<!-- navigation end -->
+    
+    
+<!-- image // banner -->
+    <?php
+        if($Page->coverImage()) {
+            $imgsrc=$Page->coverImage();
+        } else {
+            $imgsrc=HTML_PATH_THEME.'img/banner.png';
+        }
+        echo '
+        <div class="desktop" style="background-color: grey; clear: left">
+            <img class="desktop" src="'.$imgsrc.'" alt="Cover Image" style="max-height: 25rem; display: block; margin: 0 auto;">
+        </div>';
+    ?>
+<!-- image // banner end -->
+
+
+    
+    
+<!-- sub navigation -->
+    <div class="blau" style="overflow: auto; clear: left">
+        <div class="maxWidth">
+            <div class="sehtrand"> 
+                <?php
+                    // check if page has childs or is child
+                    if ( array_key_exists( $Page->key() , $pagesParents ) || $Page->parentKey() ) {
+                        // get children if Parent or siblings if child
+                        if ($Page->parentKey()) {
+                            $ParentKey = $Page->parentKey();
+                            $children = $pagesParents[$Page->parentKey()];
+                        } else {
+                            $ParentKey = $Page->key();
+                            $children = $pagesParents[$Page->key()];
+                        }
+                        foreach( $children as $Child ) {
+                            if ( substr( $Child->description(), 0, 3 ) == 'fa-' ) {
+                                $icon = 'fa '.$Child->description();
+                            } else {
+                                $icon = '';
+                            }
+                            if ( $Child->title() == $Page->title() ) {
+                                $button='sehtButton grau';
+                            } else {
+                                $button='sehtButton blau';
+                            }
+                            echo '
+                                <div style="float: left; margin: 0 0rem 0 0;">
+                                    <a class="'.$button.'" href="'.$Child->permalink().'">
+                                        <i class="fa '.$icon.'">
+                                        </i>
+                                        '.$Child->title().'
+                                    </a>
+                                </div>';
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
+<!-- sub navigation end -->
+
+
+<!-- content -->
+<div class="maxWidth" style="clear: both;">
+    <div class="sehtrand"> 
+
+
+            <div class="sehtLinks">
+                <inhalt>
+                    <?php echo $Page->content(); ?>
+                </inhalt>
+            </div>
+            <div class="sehtRechts">
+                <p style="margin-left: 1rem;">
+                    <hr>
+                    <em>SeHT Münster e.V.</em><br>
+                    - Alte Dechanei -<br>
+                    Dechaneistr. 14<br>
+                    48145 Münster
+                    <hr>
+                    <i class="fa fa-mobile"></i> 0251/136920<br>
+                    <a href="mailto: info@seht-muenster.de" style="text-decoration: none;" target="_blank"><i class="fa fa-envelope-o"></i> info@seht-muenster.de</a>
+                    <hr>
+                </p>
+            </div>
+
+    </div> <!-- margins -->
+</div>
+<!-- content end -->
+
+<!-- footer -->
+<div class="blau footer sehtHeight" style="overflow: auto; clear: left;">
+    <div class="maxWidth">
+        <div class="sehtrand"> 
+            Impressum <span style="color: rgb(188,188,188)">SeHT Münster e.V. (c) 2016</span>
+        </div>
+    </div>
+</div>
+<!-- footer end -->
+
+</body>
+</html>
